7153296acba49bfe5e9e8bc136ae49780c5726d4
ダイレクトコネクト <52444606+TokyoTF@users.noreply.github.com>
2026-04-07 19:15:39 -0400

    fix img thothub, fix page next

--- plugin.video.cumination/resources/lib/sites/thothub.py
+++ plugin.video.cumination/resources/lib/sites/thothub.py
@@ -17,11 +17,16 @@
 '''
 
 import re
+import time
+from urllib import parse as urllib_parse
 from resources.lib import utils
 from resources.lib.decrypters.kvsplayer import kvs_decode
 from resources.lib.adultsite import AdultSite
 
-site = AdultSite('thothub', '[COLOR hotpink]thothub[/COLOR]', 'https://thothub.tube/', 'https://thothub.tube/static/images/logo1colo2r.png', 'thothub')
+
+user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36'
+site = AdultSite('thothub', '[COLOR hotpink]Thothub[/COLOR]', 'https://www.thothub.tube/', 'https://www.thothub.tube/static/images/logo1colo2r.png', 'thothub')
+page = 2
 
 # ListSites
 # https://thothub.tube
@@ -29,11 +34,20 @@ site = AdultSite('thothub', '[COLOR hotpink]thothub[/COLOR]', 'https://thothub.t
 # https://thothub.lol
 # https://thothub.mx
 
+# ⚠ Spam warning: excessive requests may trigger anti-leech system and temporarily block your IP
+
+BLOCKS = {
+    'most-popular': ('list_videos_common_videos_list','video_viewed'),
+    'latest-updates': ('list_videos_latest_videos_list','post_date'),
+    'models': ('list_models_models_list','avg_videos_rating'),
+    'search': ('list_videos_videos_list_search_result', ''),
+    'none': ('list_videos_most_recent_videos','post_date')
+}
+
 @site.register(default_mode=True)
 def Main():
-    site.add_dir('[COLOR hotpink]Search[/COLOR]', site.url + 'search/?q=', 'Search', site.img_search)
+    site.add_dir('[COLOR hotpink]Search[/COLOR]', site.url + 'search/', 'Search', site.img_search)
     site.add_dir('[COLOR hotpink]Latest Updates[/COLOR]', site.url + 'latest-updates/', 'List', site.img_next)
-    site.add_dir('[COLOR hotpink]Top Rated[/COLOR]', site.url + 'top-rated/', 'List', site.img_next)
     site.add_dir('[COLOR hotpink]Most Popular[/COLOR]', site.url + 'most-popular/', 'List', site.img_next)
     site.add_dir('[COLOR hotpink]Categories[/COLOR]', site.url + 'categories/', 'Categories', site.img_cat)
     site.add_dir('[COLOR hotpink]Models[/COLOR]', site.url + 'models/', 'Models', site.img_cat)
@@ -52,12 +66,53 @@ def List(url):
     re_img = 'data-original="([^"]+)"'
     re_duration = 'class="views-counter2"[^>]*>([^<]+)'
 
-    utils.videos_list(site, 'thothub.Playvid', html, delimiter, re_videopage, re_name, re_img, re_duration=re_duration)
-    
+    for video in re.split(delimiter, html)[1:]:
+        match = re.search(re_videopage, video)
+        if not match:
+            continue
+        videopage = utils.fix_url(match.group(1), site.url)
+
+        match = re.search(re_name, video)
+        name = utils.cleantext(match.group(1)) if match else ''
+
+        match = re.search(re_img, video)
+        img = utils.fix_url(match.group(1).replace('&amp;', '&'), site.url) + '|User-Agent=' + user_agent + '|Referer=' + site.url if match else ''
+
+        match = re.search(re_duration, video)
+        duration = match.group(1) if match else ''
+
+        site.add_download_link(name, videopage, 'thothub.Playvid', img, name, duration=duration)
+
+    match = re.search(r'/(latest-updates|most-popular|models|search)/', url)
     nextp = re.compile(r'<a[^>]+href="([^"]+)"[^>]*>Next</a>', re.DOTALL).search(html)
-    if nextp:
-        site.add_dir('Next Page >>', site.url + nextp.group(1), 'List', site.img_next)
-        
+
+    if not nextp:
+        return
+
+    match_from_videos = re.search(r'&from_videos=(\d+)', url)
+    match_from = re.search(r'&from=(\d+)', url)
+    if match_from_videos:
+        page = int(match_from_videos.group(1)) + 1
+        next_url = re.sub(r'&from_videos=\d+', '&from_videos={0}'.format(page), url)
+        next_url = re.sub(r'&from_albums=\d+', '&from_albums={0}'.format(page), next_url)
+    elif match_from:
+        page = int(match_from.group(1)) + 1
+        next_url = re.sub(r'&from=\d+', '&from={0}'.format(page), url)
+    elif match:
+        page = 2
+        section = match.group(1)
+        block_id = BLOCKS[section][0]
+        if match.group(1) == 'search':
+            next_url = re.sub(r'\?.*', '', url) + '?mode=async&function=get_block&block_id={0}&q=&sort_by=&from_videos={1}&from_albums={2}&_={3}'.format(block_id, page, page, int(time.time() * 1000))
+        else:
+            next_url = nextp.group(1) + '?mode=async&function=get_block&block_id={0}&sort_by={1}&from={2}&_={3}'.format(block_id, BLOCKS[section][1], page, int(time.time() * 1000))
+    else:
+        page = 2
+        next_url = site.url + '/?mode=async&function=get_block&block_id={0}&sort_by={1}&from={2}&_={3}'.format(BLOCKS['none'][0], BLOCKS['none'][1], page, int(time.time() * 1000))
+
+    if match or match_from_videos or match_from:
+        site.add_dir('Next Page >>', next_url, 'List', site.img_next)
+
     utils.eod()
 
 
@@ -90,12 +145,22 @@ def Models(url):
             
             if match_url and match_name:
                 img = match_img.group(1) if match_img else site.img_cat
+                if img != site.img_cat:
+                    re_img_format = "{0}|{1}".format(re_img, {'Referer': site.url})
+                    img = re_img_format
                 site.add_dir(match_name.group(1), match_url.group(1), 'List', img, '')
-     
-    nextp = re.compile(r'<a[^>]+href="([^"]+)"[^>]*>Next</a>', re.DOTALL).search(html)
-    if nextp:
-        site.add_dir('Next Page >>', site.url + nextp.group(1), 'Models', site.img_next)
-    
+
+    if 'from=' in url:
+        match = re.search(r'from=(\d+)', url)
+        page = int(match.group(1)) + 1 if match else 2
+        next_url = re.sub(r'from=\d+', 'from={}'.format(page), url)
+        next_url = re.sub(r'&_=\d+', '&_={}'.format(int(time.time() * 1000)), next_url)
+        site.add_dir('Next Page >>', next_url, 'Models', site.img_next)
+    else:
+        nextp = re.compile(r'<a[^>]+href="([^"]+)"[^>]*>Next</a>', re.DOTALL).search(html)
+        if nextp:
+            site.add_dir('Next Page >>', site.url + nextp.group(1), 'Models', site.img_next)
+
     utils.eod()
 
 
@@ -104,8 +169,9 @@ def Search(url, keyword=None):
     if not keyword:
         site.search_dir(url, 'Search')
     else:
-        searchUrl = url + keyword.replace(' ', '+')
-        List(searchUrl)
+        search_term = urllib_parse.quote_plus(keyword)
+        search_url = site.url + 'search/{0}/'.format(search_term)
+        List(search_url)
 
 
 @site.register()
@@ -122,8 +188,8 @@ def Playvid(url, name, download=None):
         vu = video_url.group(1)
         
         final_url = kvs_decode(vu, lc)
-   
-        final_url += '|User-Agent={0}&Referer={1}'.format(utils.USER_AGENT, url)
+
+        final_url += '|User-Agent={0}&Referer={1}'.format(user_agent, url)
         vp.play_from_direct_link(final_url)
     else:
         vp.play_from_site_link(url + ('/' if not url.endswith('/') else ''))
